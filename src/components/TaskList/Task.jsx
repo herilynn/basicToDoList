f1b90161a85a305119b34d264a7a1aac85de42ab
Henry <4henrylin17@gmail.com>
2024-06-13 23:26:37 -0400

    Got the text to change and cross out but not change colors

--- src/components/TaskList/Task.jsx
+++ src/components/TaskList/Task.jsx
@@ -1,10 +1,25 @@
-const Task = ({name, time, complete}) => {
+import React, {useState} from "react"
+
+const Task = ({name, time, complete, id}) => {
+
+    const [isChecked, setIsChecked] = useState(false)
+
+    const handleCheckBox = () => {
+        setIsChecked(!isChecked)
+
+        const taskEle = document.querySelector('.task')
+        taskEle.classList.toggle('complete')
+        
+    }
 
     return (
         <div className="task">
+            <label>
+                <input type="checkbox" checked = {isChecked} onChange={handleCheckBox}/>
+                {isChecked ? 'Complete!' : 'Incomplete'}
+            </label>
             <div className="task-name">{name}</div>
             <div className="task-time">{time}</div>
-            <div className="completion">{complete}</div>
             <button type="submit">Remove</button>
         </div>
     )
