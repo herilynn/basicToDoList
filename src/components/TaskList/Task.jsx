c9a43492b811614ba6f9159547341b2c75d18614
Henry <4henrylin17@gmail.com>
2024-06-15 02:35:38 -0400

    Fix completion status not persisting when page refreshes

--- src/components/TaskList/Task.jsx
+++ src/components/TaskList/Task.jsx
@@ -1,19 +1,22 @@
 import React, {useState} from "react"
 
-const Task = ({name, time, id, onRemoveHandler}) => {
+const Task = ({name, time, id, completed, onRemoveHandler}) => {
 
     const RemoveHandler = () => {
         onRemoveHandler(id)
     }
 
-    const [isChecked, setIsChecked] = useState(false)
+    const [isChecked, setIsChecked] = useState(completed)
 
     const handleCheckBox = () => {
         setIsChecked(!isChecked)
 
         // const taskEle = document.querySelector('.task') DON'T USE THIS; ONLY GRABS THE 1ST TASK
         // taskEle.classList.toggle('complete')
-        
+        const updatedTasks = JSON.parse(localStorage.getItem("tasks")).map((task) =>
+            task.id === id ? { ...task, completed: !isChecked } : task
+        )
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
     }
 
     const formattedTime = new Date(time).toLocaleString()
